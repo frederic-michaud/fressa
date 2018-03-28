18ac087d7db76f8b1a514f1524319a732418e4f0
Frederic Michaud <fmichaud1@bio-51-100.unil.ch>
2018-03-28 11:37:46 +0200

    Adding documentation structure and documentation for the function mod

--- R/utilities_function.R
+++ R/utilities_function.R
@@ -1,4 +1,13 @@
-
+#' give the modulus of a number
+#'
+#' This function return the modulus of a number. The difference with
+#' the R built-in function is that the value of n\%n is n instead of 0.
+#' @return The remainder of the division of value by modulo
+#' @param value A number from which you want to compute the modulo
+#' @param modulo The value of the modulus.
+#' @examples
+#' mod(15,12)
+#' mod(12,12)
 mod <- function(value,modulo){
   new.value <- value%%modulo
   if(new.value>0) return(new.value) else return(modulo)
