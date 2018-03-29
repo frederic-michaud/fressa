00c3349ec65e7eaacfe8ddecbe1379daaf3e0ac0
Frederic Michaud <fmichaud1@bio-51-100.unil.ch>
2018-03-29 11:19:44 +0200

    Refactoring the function simulate.frequency

--- R/frequency_function.R
+++ R/frequency_function.R
@@ -1,69 +1,71 @@
+#' Get the genotype of all child from their parents
+#'
+#' This function returns a 2x4 matrix containing all the possible
+#' genotype (on each line) of the child coming from two differents
+#' parents.
+get.childs.genotype.from.parent.genotype <- function(male.genotype,female.genotype,genome){
+  haplotype.male.1 <- male.genotype[1]
+  haplotype.male.2 <- male.genotype[2]
+  haplotype.female.1 <- female.genotype[1]
+  haplotype.female.2 <- female.genotype[2]
+  a1 = c(haplotype.male.1,haplotype.female.1)
+  a2 = c(haplotype.male.1,haplotype.female.2)
+  a3 = c(haplotype.male.2,haplotype.female.1)
+  a4 = c(haplotype.male.2,haplotype.female.2)
+  return(matrix(c(a1,a2,a3,a4),ncol=2,byrow = T))
+}
+
+
+#' Get the index of all child from their parents index
+#'
+#' This function returns the index of all child, i.e. the position
+#' of this individual in the all.genotype matrix.
+get.childs.index.from.parent.index <- function(male.genotype,female.genotype,genome){
+  all.genotype <- build.all.genotype(genome)
+  childs <- get.childs.genotype.from.parent.genotype(all.genotype[male.genotype,],all.genotype[female.genotype,])
+  a1 <- get.genotype.index.from.haplotypes.index(childs[1,],all.genotype)
+  a2 <- get.genotype.index.from.haplotypes.index(childs[2,],all.genotype)
+  a3 <- get.genotype.index.from.haplotypes.index(childs[3,],all.genotype)
+  a4 <- get.genotype.index.from.haplotypes.index(childs[4,],all.genotype)
+  return(c(a1,a2,a3,a4))
+}
+
+#' Get the frequency of new born as a function of adults
+
 simulate.frequency <- function(genome,initial.frequency){
 nb.genotypes <- dim(build.all.genotype(genome))[1]
 males.genotype <- get.male(genome)
 females.genotype <- get.female(genome)
-nb.genotypes <- dim(build.all.genotype(genome))[1]
 frequencies <- initial.frequency
-new.frequencies <- rep(0,nb.genotypes)
 male.frequency <- sum(frequencies[males.genotype])
 female.frequency <- sum(frequencies[females.genotype])
 props <- c()
 all.genotype <- build.all.genotype(genome)
 all.haplotype <- build.all.haplotype(genome)
+new.frequencies <- rep(0,nb.genotypes)
+fitness.males <- sapply(1:nb.genotypes, get.fitness.from.genotype.male,genome = genome)
+fitness.females <- sapply(1:nb.genotypes, get.fitness.from.genotype.female,genome = genome)
 
-fitness.males <- rep(0,nb.genotypes)
-fitness.females <- rep(0,nb.genotypes)
-for(i in 1:nb.genotypes) fitness.males[i] <- get.fitness.from.genotype.male(i,genome,all.haplotype, all.genotype)
-for(i in 1:nb.genotypes) fitness.females[i] <- get.fitness.from.genotype.female(i,genome,all.haplotype, all.genotype)
 mean.fitness.male <- 0
 for(male.genotype in males.genotype) mean.fitness.male <- mean.fitness.male + 1/male.frequency*fitness.males[male.genotype]*frequencies[male.genotype]
-
+fitness.males <- fitness.males/mean.fitness.male
 mean.fitness.female <- 0
 for(female.genotype in females.genotype) mean.fitness.female <- mean.fitness.female + 1/female.frequency*fitness.females[female.genotype]*frequencies[female.genotype]
+fitness.females <- fitness.females/mean.fitness.female
 
-  for(male.genotype in males.genotype){
-    for(female.genotype in females.genotype){
-      haplotype.male.1 <- all.genotype[male.genotype,1]
-      haplotype.male.2 <- all.genotype[male.genotype,2]
-      haplotype.female.1 <- all.genotype[female.genotype,1]
-      haplotype.female.2 <- all.genotype[female.genotype,2]
-      a1 = c(haplotype.male.1,haplotype.female.1)
-      a2 = c(haplotype.male.1,haplotype.female.2)
-      a3 = c(haplotype.male.2,haplotype.female.1)
-      a4 = c(haplotype.male.2,haplotype.female.2)
-      fitness.male <- fitness.males[male.genotype]/mean.fitness.male
-      fitness.female <- fitness.females[female.genotype]/mean.fitness.female
-      genotype.kid <- get.genotype.index.from.haplotypes.index(a1,all.genotype)
-      new.frequencies[genotype.kid] <- new.frequencies[genotype.kid] +
-        fitness.male*fitness.female*
-        1/4*1/(male.frequency*female.frequency)*
-        (frequencies[male.genotype]*frequencies[female.genotype])
-      genotype.kid <- get.genotype.index.from.haplotypes.index(a2,all.genotype)
-      new.frequencies[genotype.kid] <- new.frequencies[genotype.kid] +
-        fitness.male*fitness.female*
-        1/4*1/(male.frequency*female.frequency)*
-        (frequencies[male.genotype]*frequencies[female.genotype])
-      genotype.kid <- get.genotype.index.from.haplotypes.index(a3,all.genotype)
-      new.frequencies[genotype.kid] <- new.frequencies[genotype.kid] +
-        fitness.male*fitness.female*
-        1/4*1/(male.frequency*female.frequency)*
-        (frequencies[male.genotype]*frequencies[female.genotype])
-      genotype.kid <- get.genotype.index.from.haplotypes.index(a4,all.genotype)
-      new.frequencies[genotype.kid] <- new.frequencies[genotype.kid] +
-        fitness.male*fitness.female*
-        1/4*1/(male.frequency*female.frequency)*
-        (frequencies[male.genotype]*frequencies[female.genotype])
+for(male.genotype in males.genotype){
+  for(female.genotype in females.genotype){
+    childs <- get.childs.index.from.parent.index(male.genotype,female.genotype,genome)
+    fitness.male <- fitness.males[male.genotype]
+    fitness.female <- fitness.females[female.genotype]
+    fitness.couple <- fitness.male*fitness.female
+    probability.of.couple <-1/(male.frequency*female.frequency)*(frequencies[male.genotype]*frequencies[female.genotype])
+    for(child in childs){
+      new.frequencies[child] <- new.frequencies[child] + fitness.couple*probability.of.couple/4
     }
   }
-
-new.frequencies
-sum(new.frequencies)
-male.frequency <- sum(new.frequencies[males.genotype])
-female.frequency <- sum(new.frequencies[females.genotype])
-male.frequency
-female.frequency
-frequencies <- new.frequencies
-return(frequencies)
+}
+return(new.frequencies)
 }
 
 
