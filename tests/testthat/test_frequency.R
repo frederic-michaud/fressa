abd2bfca235b0a3b193f7e2925138d454b57546c
Frederic Michaud <fmichaud1@bio-51-100.unil.ch>
2018-03-29 16:10:16 +0200

    Adding test for the computation of the evoluation of the frequency (simulate.frequency). I only compare to a reference value since it's very difficult to get the result in a different way.

--- tests/testthat/test_frequency.R
+++ tests/testthat/test_frequency.R
@@ -14,3 +14,11 @@ test_that("We get the correct kids from the parent (index level)",
             expect_equal(get.childs.index.from.parent.index(2, 55,genome2),c(4,7,21,24))
           }
 )
+
+test_that("Frequencies are correctly updated at each generation",
+          {
+            expect_known_value(simulate.frequency(genome1,c(0.1,0.1,0.1,0.1,0.2,0.2,0.2)),"freq_genome1.rds",update = F)
+            #expect_known_value(simulate.frequency(genome2,rep(1/171,171)),"freq_genome2.rds",update = F) #commented because too slow
+            expect_known_value(simulate.frequency(genome.partially.sexual,rep(1/19,18)),"freq_genome_partially_sexual.rds",update = F)
+          }
+)
