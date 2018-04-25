ab6f6187cd7e385330123106cb99742935dfac9d
Frederic Michaud <fmichaud1@bio-49-79.unil.ch>
2018-04-25 12:29:33 +0200

    Adding version of the modifier vignette which show how to reproduce Sally Otto's result.

--- inst/doc/modifier.R
+++ inst/doc/modifier.R
@@ -44,16 +44,26 @@ locusm = create.locus(allele1 = c(1,1,2),
                       )
 locussa = create.locus(allele1=  c(1,1,2),
                       allele2 = c(1,2,2),
-                      fitness.female = c(0.6,1,0.6),
-                      fitness.male = c(0.8,1,0.3),
-                      allele.name = c("A","a"))
+                      fitness.female = c(0.75,1,0.75),
+                      fitness.male = c(0.95,1,0.4),
+                      allele.name = c("a","A"))
 genome = create.genome(list(locussd,locussa,locusm),
-                       male.recombination = c(0.01,0.01),
-                       female.recombination = c(0.01,0.01))
+                       male.recombination = c(0.01,0.5),
+                       female.recombination = c(0.01,0.5))
 print(genome)
+get.haplotype.names(genome)
 
 ## ------------------------------------------------------------------------
-initial.frequency <- get.frequency.from.one.allele.frequency(genome,3,2,1.99)
-freq <- compute.frequency.evolution(genome, generations = 1000)
+initial.frequency <- get.frequency.from.gamete.frequency(genome,
+                                                 male.gamete.frequency = c(0.49,0,0.01,0,0.01,0,0.49,0),
+                                                 female.gamete.frequency = c(0.99,0,0.01,0,0,0,0,0))
+freq <- compute.frequency.evolution(genome,initial.frequency, generations = 1000)
 plot.haplotype.frequency(genome,freq)
+equilibrium.frequency <- freq[,ncol(freq)]
+equilibrium.frequency[8] <- 0.01 #addinf a mutation
+
+## ------------------------------------------------------------------------
+freq <- compute.frequency.evolution(genome,equilibrium.frequency, generations = 40000)
+plot.haplotype.frequency(genome,freq)
+plot.allele.frequency(genome,freq,3)
 
